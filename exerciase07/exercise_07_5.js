c32ad40697793211569df2309f1a1914336b9e60
Luann Webster <luluwebster@gmail.com>
2018-02-22 18:09:25 -0800

    exercise 07.5
    Finished exercise 07.5...The slideshow is auto and increased opacity.

--- exerciase07/exercise_07_5.js
+++ exerciase07/exercise_07_5.js
@@ -1,20 +1,22 @@
 
 /*5. Reference the Lynda.com jquery tutorial. Convert the image slide show in the tutorial to an autoplaying slide show, Create a fade translation between the slides, have each image last for three seconds, and make the overlay background more transparent. */
 
-$(function(){
-    'use strict';
+$(function() {
+"use strict";
 
-    var $form = $('#frmUser');
+	var $flowers = $('#flower-items');
 
-    $form.find('.group:nth-child(1)')
-    .addClass('error');
-
-    $form.find('.group:nth-child(3)')
-    .addClass('msg')
-    .append('<p>Good Choice!</p>');
-
+	$flowers
+    	.find('a')
+    	.colorbox({
+    		'rel' : 'gallery',
+    		'maxWidth' : '80%',
+			'transition' : 'fade',
+			'speed' : 750,
+			'opacity' : 0.2,
+			'slideshow' : true,
+            'slideshowAuto': true,
+			'slideshowSpeed' : 3000,
+			'current' : 'arrangement {current} of {total}'
+    	});
 });
-
-
-
-/*6. Add a button and an image to a page. Code the Javascript and jQuery so that when the user clicks on a button it animates the image across the screen and fades the image to 50%. Lastly it should use one form of animation easing as described in the Lynda.com tutorials.*/
